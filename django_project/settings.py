9ba9fa39b0d3ac6591e5351be52ea26ffd58b4ac
Andrew Riefenstahl <andrew.riefenstahl@gmail.com>
2022-06-26 21:16:42 -0500

    prepare schema creation

--- django_project/settings.py
+++ django_project/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     "allauth.socialaccount",
     "dj_rest_auth",
     "dj_rest_auth.registration",
+    # "drf_spectacular",
     # Local
     "accounts.apps.AccountsConfig",
     "posts.apps.PostsConfig",
@@ -168,7 +169,17 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.TokenAuthentication",
     ],
+    # "DEFAULT_SCHEMA_CLASS": [
+    #     "drf_spectacular.openapi.AutoSchema",
+    # ],
 }
 
 CORS_ALLOWED_ORIGINS = ("http://localhost:3000", "https://localhost:8000")
 CSRF_TRUSTED_ORIGINS = ["http://localhost:3000"]
+
+# SPECTACULAR_SETTINGS = {
+#     "TITLE": "Rief Productions API",
+#     "DESCRIPTION": "An API for Rief Productions",
+#     "VERSION": "1.0.0",
+#     # Other Settings
+# }
